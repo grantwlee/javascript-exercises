82bca6052da5b302e30c70c68fecd309cbe9a66d
grantwlee <121915384+grantwlee@users.noreply.github.com>
2025-11-30 11:02:06 -0600

    complete lesson 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    for(const arg of theArgs){
+        while(arr.includes(arg))
+            arr.splice(arr.indexOf(arg), 1)
+    }
+    return arr
 };
 
 // Do not edit below this line
