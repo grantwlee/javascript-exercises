4522fb4622cd9fbe7a21dbf5d1f6fa6155b6e5be
grantwlee <121915384+grantwlee@users.noreply.github.com>
2025-11-30 11:12:31 -0600

    complete lesson 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"
+    if(a<0 || b<0) return "ERROR"
+    let res = 0
+    if(a==b)
+        return a
+    else if(a>b)
+        for(let i=a; i>b-1; i--)
+            res += i
+    else if(a<b)
+        for(let i=a; i<b+1; i++)
+            res += i
+    return res        
 
 };
 
