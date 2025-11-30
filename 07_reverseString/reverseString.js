697cf353dcc4ba78093fade76ee17d04f1c778cf
grantwlee <121915384+grantwlee@users.noreply.github.com>
2025-11-30 10:52:50 -0600

    complete lesson 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    res = ""
+    for(let i=str.length-1; i>=0; i--){
+        res += str[i]
+    }
+    return res
 
 };
 
